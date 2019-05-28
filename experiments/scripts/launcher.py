8a05acd5ed2ef7f376f915e7231221fec0a58d5b
Paul Fiterau <fiteraup@gmail.com>
2019-05-28 20:06:09 +0200

    Added redirect option

--- experiments/scripts/launcher.py
+++ experiments/scripts/launcher.py
@@ -17,27 +17,33 @@ def print_usage():
 
  
 
-def launch_experiment(arg_file, jar_file, lib_dir, other_args = []):
+def launch_experiment(arg_file, jar_file, lib_dir, redir, other_args = []):
     command = COMMAND_TEMPLATE.format(jar_file, lib_dir, arg_file)
     command += " ".join(other_args)
     print("Launching ", command)
-    subprocess.Popen(command, shell=True, 
-        stdout=sys.stdout,#subprocess.DEVNULL, 
-        #stderr=subprocess.DEVNULL, 
+    if redir:
+        redir_out, redir_err=sys.stdout, sys.stderr
+    else:
+        redir_out, redir_err=subprocess.DEVNULL, subprocess.DEVNULL
+    subp = subprocess.Popen(command, shell=True, 
+        stdout=redir_out, 
+        stderr=redir_err, 
         close_fds=True)
+    print("Process pid: ", subp.pid)
 
-def launch_experiments(args_dir, jar_file, other_args = []):
+def launch_experiments(args_dir, jar_file, redir, other_args = []):
     lib_dir = os.path.join(os.path.dirname(jar_file), "lib")
     for arg_file in os.listdir(args_dir):
         if os.path.isfile:
-            launch_experiment(os.path.join(args_dir,arg_file), jar_file, lib_dir, other_args=other_args)
+            launch_experiment(os.path.join(args_dir,arg_file), jar_file, lib_dir, redir, other_args=other_args)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Utility for launching multiple instances of DTLS learner')
-    parser.add_argument('-a', "--args", required=True, type=str, help="directory with argument files")
+    parser.add_argument('-a', "--args", required=True, type=str, help="directory with argument files/argument file")
+    parser.add_argument('-r', "--redirect", action='store_true', help="redirect process output/error to stdout/stderr")
     parser.add_argument('-j', "--jar", required=False, type=str, default=JAR_LOCATION, help="DTLS learner .jar file, note that a lib dir should be in the same directory as the .jar ")
     parser.add_argument('-p', "--params", required=False, nargs='+', default=[], help="additional parameters to pass to the learning processes (which augment/overwrite those in the argument files)")
     args = parser.parse_args()
     print(args.params)
-    launch_experiments(args.args, args.jar, other_args=args.params)
+    launch_experiments(args.args, args.jar, args.redirect, other_args=args.params)
