5786f90e65b65da8b68298523e1aaef1097e0fbf
Paul Fiterau <fiteraup@gmail.com>
2019-07-15 15:53:51 +0200

    Added resume option to client hello input

--- src/main/java/se/uu/it/dtlsfuzzer/sut/io/ClientHelloInput.java
+++ src/main/java/se/uu/it/dtlsfuzzer/sut/io/ClientHelloInput.java
@@ -8,6 +8,7 @@ import de.rub.nds.tlsattacker.core.constants.CipherSuite;
 import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
 import de.rub.nds.tlsattacker.core.protocol.message.ProtocolMessage;
 import de.rub.nds.tlsattacker.core.state.State;
+import de.rub.nds.tlsattacker.core.workflow.action.ResetConnectionAction;
 
 public class ClientHelloInput extends NamedTlsInput {
 
@@ -26,6 +27,13 @@ public class ClientHelloInput extends NamedTlsInput {
 		this.suite = cipherSuite;
 	}
 
+	private void resetTransportHandler(State state) {
+		ResetConnectionAction resetAction = new ResetConnectionAction();
+		resetAction.setConnectionAlias(state.getTlsContext().getConnection()
+				.getAlias());
+		resetAction.execute(state);
+	}
+
 	@Override
 	public ProtocolMessage generateMessage(State state) {
 		state.getConfig().setDefaultClientSupportedCiphersuites(
@@ -33,6 +41,14 @@ public class ClientHelloInput extends NamedTlsInput {
 		state.getTlsContext().getDigest().reset();
 		ClientHelloMessage message = new ClientHelloMessage(state.getConfig());
 		if (resume && !state.getTlsContext().getSessionList().isEmpty()) {
+			// resume and reset the connection
+			resetTransportHandler(state);
+			state.getTlsContext().setDtlsCookie(new byte[]{});
+			state.getTlsContext().setDtlsNextReceiveSequenceNumber(0);
+			state.getTlsContext().setDtlsNextSendSequenceNumber(0);
+			state.getTlsContext().setDtlsSendEpoch(0);
+			state.getTlsContext().setDtlsNextReceiveEpoch(0);
+			message.setCookie(new byte[]{});
 			message.setSessionId(state.getTlsContext().getSessionList()
 					.get(state.getTlsContext().getSessionList().size() - 1)
 					.getSessionId());
