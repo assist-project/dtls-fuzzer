e76f89833c8a6416b8a33d0b69b684c70ff7d9f6
Paul Fiterau <fiteraup@yahoo.com>
2022-05-20 13:54:54 +0200

    Update grep stats script

--- experiments/scripts/grep_stats.sh
+++ experiments/scripts/grep_stats.sh
@@ -2,6 +2,8 @@ readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 &&
 STATS_FILENAME="statistics.txt"
 if [ $# = 0 ]; then
     echo "Usage: grep_stats.sh dir"
+    echo ""
+    echo "dir - directory containing output folders resulting from learning experiments"
     exit
 fi
 
@@ -21,12 +23,13 @@ function grep_timed_stat() {
   done
 }
 
-echo "Grep-ing stats to be introduced in the table, ensure that the results folders are ordered in the same way as table rows"
+echo "Grep-ing stats to be directory $models_dir"
 echo Implementations
+
 ls -1 $models_dir/*/$STATS_FILENAME | xargs -i dirname {} | xargs -i basename {}
 grep_stat "state count" "Number of states"
-#grep_stat "hyp count" "Number of hypotheses"
-#grep_stat "total tests" "Number of resets"
-#grep_stat "total learning tests" "Number of learning resets"
-#grep_stat "total tests to last hyp" "Number of resets up to last hyp"
-#grep_timed_stat "time" "Time it took"
+grep_stat "hyp count" "Number of hypotheses"
+grep_stat "total tests" "Number of tests"
+grep_stat "total learning tests" "Number of learning tests"
+grep_stat "total tests to last hyp" "Number of tests up to last hyp"
+grep_timed_stat "time" "Time it took"
