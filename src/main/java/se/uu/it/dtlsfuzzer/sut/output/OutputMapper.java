bca1a3c681af5f16eabd30a0eaf964fe2c6074a0
Kostis Sagonas <kostis@it.uu.se>
2022-05-23 19:03:59 +0200

    Fix an import

--- src/main/java/se/uu/it/dtlsfuzzer/sut/output/OutputMapper.java
+++ src/main/java/se/uu/it/dtlsfuzzer/sut/output/OutputMapper.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang3.NotImplementedException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bouncycastle.asn1.ASN1InputStream;
