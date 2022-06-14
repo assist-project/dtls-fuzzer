6bb1eb11ccaf98095a9ddcb709eadf85e061a5b8
Paul Fiterau <fiteraup@yahoo.com>
2022-06-14 10:34:36 +0200

    For now disabled encryption setting in DtlsInput

--- src/main/java/se/uu/it/dtlsfuzzer/sut/input/DtlsInput.java
+++ src/main/java/se/uu/it/dtlsfuzzer/sut/input/DtlsInput.java
@@ -2,16 +2,23 @@ package se.uu.it.dtlsfuzzer.sut.input;
 
 import javax.xml.bind.annotation.XmlAttribute;
 
+import org.apache.commons.lang3.NotImplementedException;
+
 import de.rub.nds.tlsattacker.core.state.State;
-import de.rub.nds.tlsattacker.core.workflow.action.ActivateEncryptionAction;
-import de.rub.nds.tlsattacker.core.workflow.action.DeactivateEncryptionAction;
+import de.rub.nds.tlsattacker.core.state.TlsContext;
 import se.uu.it.dtlsfuzzer.mapper.ExecutionContext;
 
 public abstract class DtlsInput extends TlsInput {
 	
+    /*
+     * For the given message, modifies epoch and corresponding cipher to use for encryption.
+     */
 	@XmlAttribute(name = "epoch", required = false)
 	private Integer epoch = null;
 
+	/*
+     * Enable encryption for message (only applies if current epoch > 0).
+     */
 	@XmlAttribute(name = "encryptedEnabled", required = false)
 	private boolean encryptionEnabled = true;
 
@@ -29,19 +36,21 @@ public abstract class DtlsInput extends TlsInput {
 	
 	public final void preSendUpdate(State state, ExecutionContext context) {
 		alias = context.getSulDelegate().getRole();
+
 		// if different epoch than current, set the epoch in TLS context
-		if (epoch != null && epoch != state.getTlsContext().getDtlsWriteEpoch()) {
-			contextEpoch = state.getTlsContext().getDtlsWriteEpoch();
-			state.getTlsContext().setDtlsWriteEpoch(epoch);
-			// if epoch > 0, must deactivate encryption
-			if (contextEpoch > 0  && !encryptionEnabled) {
-				DeactivateEncryptionAction action = new DeactivateEncryptionAction();
-				action.setConnectionAlias(alias);
-				action.execute(state);
-				contextWriteNumber = state.getTlsContext().getWriteSequenceNumber();
-				state.getTlsContext().setWriteSequenceNumber(context.incrementWriteRecordNumberEpoch0());
-			}
+		if (epoch != null && epoch != state.getTlsContext().getWriteEpoch()) {
+			state.getTlsContext().setWriteEpoch(epoch);
+			contextEpoch = state.getTlsContext().getWriteEpoch();
 		}
+		
+		// if epoch > 0, deactivate encryption
+        if (contextEpoch > 0  && !encryptionEnabled) {
+            throw new NotImplementedException("Disabling encryption is not currently supported.");
+//            DeactivateEncryptionAction action = new DeactivateEncryptionAction();
+//            action.setConnectionAlias(alias);
+//            action.execute(state);
+//            state.getTlsContext().setWriteSequenceNumber(context.incrementWriteRecordNumberEpoch0());
+        }
 		
 		preSendDtlsUpdate(state, context);
 	}
@@ -52,17 +61,19 @@ public abstract class DtlsInput extends TlsInput {
 	public final void postSendUpdate(State state, ExecutionContext context) {
 		// reset epoch number and, if original epoch > 0, reactivate encryption
 		if (contextEpoch != null) {
-			state.getTlsContext().setDtlsWriteEpoch(contextEpoch);
-			if (contextEpoch > 0  && !encryptionEnabled) {
-				ActivateEncryptionAction action = new ActivateEncryptionAction();
-				action.setConnectionAlias(alias);
-				action.execute(state);
-				if (contextWriteNumber != null) {
-					state.getTlsContext().setWriteSequenceNumber(contextWriteNumber);
-				}
-			}
-			contextEpoch = null;
+			state.getTlsContext().setWriteEpoch(contextEpoch);
 		}
+		contextEpoch = null;
+	    
+    	if (contextEpoch > 0  && !encryptionEnabled) {
+    	    throw new NotImplementedException("Re-enabling encryption is not currently supported.");
+//            ActivateEncryptionAction action = new ActivateEncryptionAction();
+//            action.setConnectionAlias(alias);
+//            action.execute(state);
+//            if (contextWriteNumber != null) {
+//                state.getTlsContext().setWriteSequenceNumber(contextWriteNumber);
+//            }
+    	}
 		
 		postSendDtlsUpdate(state, context);
 	}
@@ -85,5 +96,9 @@ public abstract class DtlsInput extends TlsInput {
 
 	public void setEncryptionEnabled(boolean encrypted) {
 		this.encryptionEnabled = encrypted;
+	}
+	
+	private long getWriteSequenceNumber(TlsContext context) {
+	    return context.getRecordLayer().getEncryptor().getRecordCipher(epoch).getState().getWriteSequenceNumber();
 	}
 }
