9cae5a2bbe2c54836d02ab3625cd851a9247844c
Kostis Sagonas <kostis@it.uu.se>
2023-02-09 22:18:00 +0100

    Remove an unused import [skip ci]

--- src/main/java/se/uu/it/dtlsfuzzer/learner/Learner.java
+++ src/main/java/se/uu/it/dtlsfuzzer/learner/Learner.java
@@ -7,7 +7,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
