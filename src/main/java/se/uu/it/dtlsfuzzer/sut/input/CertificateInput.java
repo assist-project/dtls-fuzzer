ffdf3eaaaae8363068acf2ea5ad0a87586a2acaf
Paul Fiterau <fiteraup@yahoo.com>
2023-05-25 14:41:47 +0200

    Alternative fix: define class for Certificate messages.

---
+++ src/main/java/se/uu/it/dtlsfuzzer/sut/input/CertificateInput.java
@@ -0,0 +1,34 @@
+package se.uu.it.dtlsfuzzer.sut.input;
+
+import java.util.Collections;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+import de.rub.nds.tlsattacker.core.protocol.message.CertificateMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.TlsMessage;
+import de.rub.nds.tlsattacker.core.state.State;
+import se.uu.it.dtlsfuzzer.mapper.ExecutionContext;
+
+public class CertificateInput extends DtlsInput {
+    
+    @XmlAttribute(name="empty", required=false)
+    private boolean empty = false;
+    
+    public CertificateInput() {
+    }
+
+    @Override
+    public TlsMessage generateMessage(State state, ExecutionContext context) {
+        CertificateMessage message = new CertificateMessage();
+        if (empty) {
+            message.setCertificateListConfig(Collections.emptyList());
+        }
+        return message;
+    }
+
+    @Override
+    public TlsInputType getInputType() {
+        return TlsInputType.HANDSHAKE;
+    }
+
+}
