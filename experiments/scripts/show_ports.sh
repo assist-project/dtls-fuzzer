4a1d6187c7fa8759c4aa3705845db5ef7c6d902d
Kostis Sagonas <kostis@it.uu.se>
2023-07-23 15:14:33 +0200

    Cleanup five shell scripts under experiments/scripts

--- experiments/scripts/show_ports.sh
+++ experiments/scripts/show_ports.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 if [[ $# -lt 2 ]]; then
     echo "Usage show_ports.sh args_dir [match_strings*]"
     exit
 fi
 
-for args_file in $1/learn*; do
+for args_file in "$1"/learn*; do
     echo "File $args_file"
     for ((i = 2 ; i <= $# ; i++)); do
         echo $i
         match_string="${!i}"
-        port=$(grep -A1 $match_string $args_file | grep -o [0-9][0-9][0-9]* | head -n 1)
+        port=$(grep -A1 "$match_string" "$args_file" | grep -o "[0-9][0-9][0-9]*" | head -n 1)
         echo "Port $port"
     done
 done
--- experiments/scripts/timestamp_hyp.sh
+++ experiments/scripts/timestamp_hyp.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 learning_folder=$1
 result_folder=$2
 prefix=$3
@@ -8,19 +10,19 @@ if [ $# != 3 ]; then
 fi
 
 if [[ ! -d $result_folder ]]; then
-    mkdir $result_folder
+    mkdir "$result_folder"
 fi
 
-start_time=$(stat --format %W $learning_folder/alphabet.xml)
+start_time=$(stat --format %W "$learning_folder"/alphabet.xml)
 hour_index=0
 
-for hyp in $learning_folder/hyp* 
+for hyp in "$learning_folder"/hyp* 
 do
-    hyp_gen_time=$(stat --format %W $hyp)
-    hours_passed=$(($hyp_gen_time/3600-$start_time/3600))
-    if (( $hours_passed > $hour_index )); then
+    hyp_gen_time=$(stat --format %W "$hyp")
+    hours_passed=$((hyp_gen_time/3600-start_time/3600))
+    if (( hours_passed > hour_index )); then
         hour_index=$hours_passed
-        ts_hyp_path=$result_folder/$prefix"_$hours_index".dot
-        cp $hyp $ts_hyp_path
+        ts_hyp_path=$result_folder/$prefix"_$hour_index".dot
+        cp "$hyp" "$ts_hyp_path"
     fi
 done
