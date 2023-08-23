7517fcbe3fc003a68ed96017f4eb502f5e1443b3
Kostis Sagonas <kostis@it.uu.se>
2023-08-23 15:01:27 +0200

    Revert last change

--- src/main/java/se/uu/it/dtlsfuzzer/Main.java
+++ src/main/java/se/uu/it/dtlsfuzzer/Main.java
@@ -11,10 +11,10 @@ public class Main {
     public static void main(String args[]) throws IOException, XMLStreamException {
         Security.addProvider(new BouncyCastleProvider());
         MultiBuilder mb = new MultiBuilder();
-        String[] parentLoggers = {Main.class.getPackageName()};
+        // String[] parentLoggers = {Main.class.getPackageName()};
 
-        CommandLineParser commandLineParser = new CommandLineParser(mb, mb, mb, mb);
-        commandLineParser.setExternalParentLoggers(parentLoggers);
+        CommandLineParser commandLineParser = new CommandLineParser(mb, mb, mb, mb, new String [] {Main.class.getPackageName()});
+        // commandLineParser.setExternalParentLoggers(parentLoggers);
         commandLineParser.parse(args);
     }
 }
