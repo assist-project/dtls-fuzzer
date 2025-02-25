db54069e2e1a62a25f5e1be5301e42230a54b800
Kostis Sagonas <kostis@it.uu.se>
2025-02-25 07:34:32 +0100

    Remove BouncyCastle

--- src/main/java/se/uu/it/dtlsfuzzer/Main.java
+++ src/main/java/se/uu/it/dtlsfuzzer/Main.java
@@ -2,14 +2,12 @@ package se.uu.it.dtlsfuzzer;
 
 import com.github.protocolfuzzing.protocolstatefuzzer.entrypoints.CommandLineParser;
 import java.io.IOException;
-import java.security.Security;
 import javax.xml.stream.XMLStreamException;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Main {
 
     public static void main(String args[]) throws IOException, XMLStreamException {
-        Security.addProvider(new BouncyCastleProvider());
+        // Security.addProvider(new BouncyCastleProvider());
         MultiBuilder mb = new MultiBuilder();
         // String[] parentLoggers = {Main.class.getPackageName()};
 
