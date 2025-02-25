13c01cfa03b09bc02bb95be9897da507d17bebf0
Kostis Sagonas <kostis@it.uu.se>
2025-02-25 14:20:31 +0100

    Eliminate dependency to org.bouncycaste
    Use the TLS-Attacker Provider API instead.

--- src/main/java/se/uu/it/dtlsfuzzer/Main.java
+++ src/main/java/se/uu/it/dtlsfuzzer/Main.java
@@ -1,15 +1,14 @@
 package se.uu.it.dtlsfuzzer;
 
 import com.github.protocolfuzzing.protocolstatefuzzer.entrypoints.CommandLineParser;
+import de.rub.nds.tlsattacker.core.util.ProviderUtil;
 import java.io.IOException;
-import java.security.Security;
 import javax.xml.stream.XMLStreamException;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Main {
 
     public static void main(String args[]) throws IOException, XMLStreamException {
-        Security.addProvider(new BouncyCastleProvider());
+        ProviderUtil.addBouncyCastleProvider();
         MultiBuilder mb = new MultiBuilder();
         // String[] parentLoggers = {Main.class.getPackageName()};
 
