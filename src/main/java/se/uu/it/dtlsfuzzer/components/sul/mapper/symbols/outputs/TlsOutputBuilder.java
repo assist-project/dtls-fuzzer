b27e6f531aa9b1a18f5ab0e8e1dc85ad0de04072
pfg666 <fiteraup@yahoo.com>
2025-03-24 12:06:07 +0100

    Update DTLS-Fuzzer to use latest PSF.

--- src/main/java/se/uu/it/dtlsfuzzer/components/sul/mapper/symbols/outputs/TlsOutputBuilder.java
+++ src/main/java/se/uu/it/dtlsfuzzer/components/sul/mapper/symbols/outputs/TlsOutputBuilder.java
@@ -2,14 +2,14 @@ package se.uu.it.dtlsfuzzer.components.sul.mapper.symbols.outputs;
 
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.OutputBuilder;
 
-public class TlsOutputBuilder implements OutputBuilder<TlsOutput> {
+public class TlsOutputBuilder extends  OutputBuilder<TlsOutput> {
 
     public TlsOutputBuilder() {
         super();
     }
 
     @Override
-    public TlsOutput buildOutput(String name) {
+    public TlsOutput buildOutputExact(String name) {
         return new TlsOutput(name);
     }
 }
