61c4963e6eb856b528d2e2622984de7ce1beeff0
Nanxi Chen <nanxichen1998@gmail.com>
2025-04-15 14:46:25 +0200

    Revert "record LastClientHelloCleanProtocolMessageBytes in TLS-Attacker"
    This reverts commit f0e024225f285fbff47e2c3026c516b38743fd3b.

--- src/main/java/se/uu/it/dtlsfuzzer/components/sul/mapper/symbols/inputs/ServerHelloInput.java
+++ src/main/java/se/uu/it/dtlsfuzzer/components/sul/mapper/symbols/inputs/ServerHelloInput.java
@@ -90,8 +90,7 @@ public class ServerHelloInput extends DtlsInput {
                 byte[] hrBytes = context.getStepContext(lastChStepIndex).getReceivedRecords().get(0).getCleanProtocolMessageBytes().getValue();
                 context.getTlsContext().getDigest().append(hrBytes);
             }
-//            byte[] chBytes = lastChPair.getRight().getCleanProtocolMessageBytes().getValue();
-            byte[] chBytes = context.getTlsContext().getLastClientHelloCleanProtocolMessageBytes();
+            byte[] chBytes = lastChPair.getRight().getCleanProtocolMessageBytes().getValue();
             byte[] shBytes = context.getStepContext().getSentRecords().get(0).getCleanProtocolMessageBytes().getValue();
             context.getTlsContext().getDigest().append(chBytes);
             context.getTlsContext().getDigest().append(shBytes);
