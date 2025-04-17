e37a17ee3ec6151edbb893d104b03b31e5e7062c
Oskar Pettersson Löfstedt <oskar.petterssonlofstedt.4134@student.uu.se>
2025-04-17 10:44:09 +0200

    Create RA sul and sulbuilder

---
+++ src/main/java/se/uu/it/dtlsfuzzer/components/sul/core/TlsSulBuilderRA.java
@@ -0,0 +1,23 @@
+package se.uu.it.dtlsfuzzer.components.sul.core;
+
+import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.AbstractSul;
+import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.SulBuilder;
+import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulConfig;
+import com.github.protocolfuzzing.protocolstatefuzzer.utils.CleanupTasks;
+import de.learnlib.ralib.words.PSymbolInstance;
+import se.uu.it.dtlsfuzzer.components.sul.mapper.TlsExecutionContextRA;
+
+public class TlsSulBuilderRA
+    implements
+        SulBuilder<PSymbolInstance, PSymbolInstance, TlsExecutionContextRA> {
+
+    @Override
+    public AbstractSul<
+        PSymbolInstance,
+        PSymbolInstance,
+        TlsExecutionContextRA
+    > build(SulConfig sulConfig, CleanupTasks cleanupTasks) {
+        TlsSul sul = new TlsSulBuilder().build(sulConfig, cleanupTasks);
+        return new TlsSulRA(sul);
+    }
+}
