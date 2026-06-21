99e719c9a18bccdef3d774f1802da8f8640ba76e
Angeliki <antalaper@gmail.com>
2026-06-21 23:32:19 +0300

    Update PSF commit

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ readonly SCRIPT_DIR
 readonly PATCHES_DIR="${SCRIPT_DIR}/experiments/patches"
 
 
-readonly PROTOCOLSTATEFUZZER_COMMIT="4848cdfc53dfcc41747691d00d07e3c4dd0acde0"
+readonly PROTOCOLSTATEFUZZER_COMMIT="c991e4c7e94308698591f2f8708b7c86eed2ea00"
 readonly PROTOCOLSTATEFUZZER_REP_URL="https://github.com/protocol-fuzzing/protocol-state-fuzzer.git"
 readonly PROTOCOLSTATEFUZZER_FOLDER="ProtocolState-Fuzzer"
 
